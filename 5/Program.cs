48a3d88c2ca3a634d74a6fec2556736dae2596e2
Tolik <anatolijparygin988@gmail.com>
2022-11-25 16:44:22 +0300

    Условие if else

---
+++ 5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "толик")
+{
+    Console.WriteLine ("Ура!Ура!Ура! Это " + username);
+}
+else
+{
+    Console.WriteLine("Привет " + username);
+}
